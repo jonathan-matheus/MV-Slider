e22b5537b10233f71b40444efa0498e95f17326d
Jonathan Matheus Da Silva <johhn.dev@gmail.com>
2024-05-24 16:34:38 -0300

    Arquivos cirados: - index.php - mv-slider
    index.php - Arquivo em branco, por questões de segurança.
    mv-slider.php - Arquivo base do plugin.

---
+++ mv-slider.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Plugin Name: MV Slider
+ * Description: Um simples plugin de slider para WordPress
+ * Version: 1.0.0
+ * Requires at least: 5.6
+ * Author: Jonathan Matheus
+ * Author URI: https://github.com/jonathan-matheus
+ * License: GPLv2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: mv-slider
+ * Domain Path: /languages
+ */
+
+/*
+MV Slider is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+MV Slider is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MV Slider. If not, see http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// Caso o código esteja sendo executado diretamente, aborte a execução
+if (!defined('ABSPATH')) {
+    exit;
+}
