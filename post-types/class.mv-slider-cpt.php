1501d92aa490660349d078c5f8c98387d07abf51
Jonathan Matheus Da Silva <johhn.dev@gmail.com>
2024-06-23 18:57:48 -0300

    Criando o tipo de post customizado(CPT) para o plugin - parte 1

---
+++ post-types/class.mv-slider-cpt.php
@@ -0,0 +1,16 @@
+<?php
+if (!class_exists('MV_Slider_Post_Type')) {
+    class MV_Slider_Post_Type
+    {
+        function __construct()
+        {
+            add_action('init', [
+                $this, 'create_post_type'
+            ]);
+        }
+
+        public function create_post_type()
+        {
+        }
+    }
+}
