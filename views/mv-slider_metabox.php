564a53a426e6ad7500eb9cebdaf23582b3931f86
Jonathan Matheus Da Silva <johhn.dev@gmail.com>
2024-06-26 16:58:24 -0300

    API Metabox - Escapando informações

--- views/mv-slider_metabox.php
+++ views/mv-slider_metabox.php
@@ -1,6 +1,9 @@
 <?php
-$slider_text = get_post_meta($post->ID, 'mv_slider_link_text', true);
-$slider_url = get_post_meta($post->ID, 'mv_slider_link_url', true);
+$slider_text = esc_html(get_post_meta($post->ID, 'mv_slider_link_text', true));
+$slider_text = isset($slider_text) ? $slider_text : '';
+
+$slider_url = esc_url(get_post_meta($post->ID, 'mv_slider_link_url', true));
+$slider_url = isset($slider_url) ? $slider_url : '';
 ?>
 
 <table class="form-table mv-slider-metabox">
@@ -9,7 +12,7 @@ $slider_url = get_post_meta($post->ID, 'mv_slider_link_url', true);
             <label for="mv_slider_link_text">Link Text</label>
         </th>
         <td>
-            <input type="text" name="mv_slider_link_text" id="mv_slider_link_text" class="regular-text link-text" value="<?= $slider_text ?>" required>
+            <input type="text" name="mv_slider_link_text" id="mv_slider_link_text" class="regular-text link-text" value="<?= $slider_text ?>">
         </td>
     </tr>
     <tr>
