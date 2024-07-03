ff05b59cc79ea53f4dce98fbb83ed1f1cfae94f8
Jonathan Matheus Da Silva <johhn.dev@gmail.com>
2024-07-03 15:18:09 -0300

    Setting + Options API - Adicionando seções e campos (parte 2)

--- views/settings-page.php
+++ views/settings-page.php
@@ -7,7 +7,11 @@ Essa div com a classe wrap e uma div padrão de toda página de plugin
     O formulário deve sempre enviar para o arquivo options.php que e um arquivo
     padrão do WordPress para processamento de dados do formulário
     -->
-    <form action="options.php">
-
+    <form action="options.php" method="post">
+        <?php
+        settings_fields('mv_slider_group');
+        do_settings_sections('mv_slider_page1');
+        submit_button('Save Settings');
+        ?>
     </form>
 </div>
